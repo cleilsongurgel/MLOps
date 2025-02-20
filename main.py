aede91708743f7bb61ed1ead4281259a1f75dd31
cleilson.gurgel <cleilsoncgb@gmail.com>
2025-02-19 22:12:52 -0300

    adicionando novo endpoit

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from textblob import TextBlob
+from googletrans import Translator
 
-
-app = Flask('meu_app')
+app = Flask('__name__')
 
 @app.route('/')
 def home():
     return 'Olá, mundo!'
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    translator = Translator()
+    tb_en = translator.translate(str(tb), dest='en').text
+
+    #print(tb_en)
+    # Create a TextBlob object from the translated text
+    tb_en_blob = TextBlob(tb_en)
+    polaridade  = tb_en_blob.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+
+app.run(debug = True)
